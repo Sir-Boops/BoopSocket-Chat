b431c871ed4c2514790dfd635e5c37921fb376aa
Sir_Boops <admin@boops.me>
2017-03-13 16:37:45 -0600

    Moved over to sqlite!

--- commands/login.js
+++ commands/login.js
@@ -1,6 +1,9 @@
 //Node.JS Imports
-var mysql = require('mysql');
 var uuid = require('uuid');
+var sqlite3 = require('sqlite3').verbose();
+
+// Load the DB
+var db = new sqlite3.Database('./dbs/users.sql', 'OPEN_READWRITE | OPEN_CREATE');
 
 //Local imports
 var config = require('../config.json');
@@ -22,62 +25,18 @@ var res = function(msg) {
 				return "/help login";
 			} else {
 
-				// Connect to the MYSQL Server
-				var conn = mysql.createConnection({
-					host: config.mysql.address,
-					user: config.mysql.username,
-					password: config.mysql.password,
-					database: config.mysql.database
+				// Create a UUID
+				db.run('INSERT INTO users VALUES ($uuid, $username, $password)',{
+
+					$uuid: (uuid.v4()),
+					$username: msg[2],
+					$password: msg[3]
+
 				});
 
-				// Start the connection
-				conn.connect(function(err){
-					if(err) {
-						console.log(err);
-					};
-
-					// Create the account
-					gen_uuid(conn, msg[2], msg[3]);
-				});
 			};
 		};
 	};
-};
-
-function gen_uuid(conn, name, password) {
-
-	// Get a random UUID
-	var user_uuid = uuid.v4();
-
-	//Ask the database
-	var query = conn.query('SELECT UUID FROM users WHERE UUID like ' + mysql.escape(user_uuid), function(err, row, fie){
-
-		//if ERR
-		if(err) {
-			console.log(err);
-		} else {
-
-			//Check for a row
-			if(row > 0) {
-				// UUID Found!
-				gen_uuid(conn);
-			} else {
-				check_username(conn, name, password, user_uuid);
-			};
-		};
-	});
-};
-
-
-function check_username(conn, name, password, UUID) {
-
-	// Insert into the table
-	conn.query('INSERT INTO users (name, UUID, password) VALUES (' + mysql.escape(name) + ', ' + mysql.escape(UUID) + ', ' + mysql.escape(password) + ')', function(err, row, fei){
-
-		if(err) {
-			console.log(err);
-		};
-	});
 };
 
 module.exports.res = res;
